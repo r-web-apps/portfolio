64f974a6b58524c0b25050f017a687a0ef361a38
Ruben Vitt <dev@rubeen.dev>
2020-05-12 13:43:18 +0200

    feat(frontend): change pagination number

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     page = params[:page]
-    if user_signed_in?
-      per = 8
-    else
-      per = 9
-    end
+    per = if user_signed_in? 5
+          else
+            6
+          end
     @portfolio_list = Portfolio.order('year DESC, month DESC').page(page).per(per)
     @comments = Comment.order('created_at').limit(10).reverse
   end
