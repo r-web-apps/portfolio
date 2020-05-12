72d83cfa0099afe95b0ae6772bb6313752aa4c38
Ruben Vitt <dev@rubeen.dev>
2020-05-12 09:13:15 +0200

    feat(portfolio): add if to migration scrip

--- db/migrate/20200511200046_rename_pk.rb
+++ db/migrate/20200511200046_rename_pk.rb
@@ -1,5 +1,5 @@
 class RenamePk < ActiveRecord::Migration[6.0]
   def change
-    rename_column :comments, :portfolios_id, :portfolio_id
+    rename_column :comments, :portfolios_id, :portfolio_id if column_exists? :comments, :portfolios_id
   end
 end
